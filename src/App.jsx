a638087d04db69ef122916617068bf8a881f3743
Claymachinegun <cl4ymachinegun@gmail.com>
2023-04-09 17:28:47 +0700

    prettify, lint, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,107 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Board from './Board.jsx'
+import React, { useState, useEffect } from "react";
+import Board from "./Board.jsx";
 
 function App() {
   const [position, setPosition] = useState({
-      "pos": {"id":0, "moveSide":"WHITE", "position":"q7/8/8/8/8/8/8/7Q"},
-      "checkResults": {"correct" : 1, "realEval" : 0 },
-      "showResults": 0,
+    pos: { id: 0, moveSide: "WHITE", position: "q7/8/8/8/8/8/8/7Q" },
+    checkResults: { correct: 1, realEval: 0 },
+    showResults: 0,
   });
 
-  const next = (()=>{
+  const next = () => {
     return fetch("http://localhost:3000/api/random", {
-        "cache": "no-cache"
+      cache: "no-cache",
+    }).then((response) => response.json());
+  };
+
+  const btnAction = (evaluation) => {
+    fetch("http://localhost:3000/api/check", {
+      method: "POST",
+      body: JSON.stringify({
+        id: position.pos.id,
+        eval: evaluation,
+      }),
     })
-          .then(response => response.json());
-  });
+      .then((response) => response.json())
+      .then((body) => {
+        setPosition({
+          ...position,
+          checkResults: body,
+          showResults: 1,
+        });
+      });
+  };
 
- const btnAction = (evaluation) => {
-      fetch("http://localhost:3000/api/check", {
-        "method": "POST",
-        "body" : JSON.stringify({
-            "id": position.pos.id,
-            "eval" : evaluation
-        })
-      })
-        .then(response=>response.json())
-         .then((body) => {
-         setPosition( {... position,
-        "checkResults" : body,
-        "showResults":1
-     });
-     });
- }
- 
- const nextButton = (e) => {
-    next().then((body) => setPosition({...position, "showResults":0, "pos":body}));
- }
-
+  const nextButton = (e) => {
+    next().then((body) =>
+      setPosition({ ...position, showResults: 0, pos: body })
+    );
+  };
 
   useEffect(() => {
     let mounted = true;
-    next()
-      .then(body => {
-        if(mounted) {
-          setPosition({...position, "showResults":0, "pos": body});
-        }
-      })
-    return () => mounted = false;
-    }, []);
+    next().then((body) => {
+      if (mounted) {
+        setPosition({ ...position, showResults: 0, pos: body });
+      }
+    });
+    return () => {
+      mounted = false;
+      return 1;
+    };
+  }, []);
 
   return (
     <div className="grid">
-      <Board whiteMove={`${position.pos.moveSide == "WHITE" ? 1 : 0}`} position={position.pos.position} />
-        <div className="grid">
-      {position.showResults == 1 ?
-
-      <div className={`place-self-center border px-4 py-3 rounded relative w-2/6 ${position.checkResults.correct == 1 ? "bg-green-100 border-green-400 text-green-700" : "bg-red-100 border-red-400 text-red-700"}`}>
-       <strong className="place-self-center">{position.checkResults.correct == 1 ? "Correct!" : "Wrong!"}</strong>
-       <p>It is {position.checkResults.realEval}</p>
+      <Board
+        whiteMove={position.pos.moveSide === "WHITE" ? 1 : 0}
+        position={position.pos.position}
+      />
+      <div className="grid">
+        {position.showResults === 1 ? (
+          <div
+            className={`place-self-center border px-4 py-3 rounded relative w-11/12 lg:w-auto ${
+              position.checkResults.correct === 1
+                ? "bg-green-100 border-green-400 text-green-700"
+                : "bg-red-100 border-red-400 text-red-700"
+            }`}
+          >
+            <strong className="place-self-center">
+              {position.checkResults.correct === 1 ? "Correct!" : "Wrong!"}
+            </strong>
+            <p>It is {position.checkResults.realEval}</p>
+          </div>
+        ) : (
+          <div className="place-self-center">
+            <button
+              className="btn btn-white w-3/12 lg:w-auto"
+              onClick={(e) => btnAction(1)}
+            >
+              White
+            </button>
+            <button
+              className="btn btn-draw w-3/12 lg:w-auto"
+              onClick={(e) => btnAction(0)}
+            >
+              Draw
+            </button>
+            <button
+              className="btn btn-black w-3/12 lg:w-auto"
+              onClick={(e) => btnAction(-1)}
+            >
+              Black
+            </button>
+          </div>
+        )}
+        <button
+          className="place-self-center btn btn-default w-11/12 lg:w-auto"
+          onClick={nextButton}
+        >
+          Next
+        </button>
       </div>
-
-          :
-
-      <div className="place-self-center">
-            <button className="btn btn-white w-3/12 lg:w-auto" onClick={(e) => btnAction(1)}>White</button>
-            <button className="btn btn-draw w-3/12 lg:w-auto" onClick={(e) => btnAction(0)}>Draw</button>
-            <button className="btn btn-black w-3/12 lg:w-auto" onClick={(e) => btnAction(-1)}>Black</button>
-      </div>
-      
-      }
-            <button className="place-self-center btn btn-default w-11/12 lg:w-auto" onClick={nextButton}>Next</button>
-        </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,63 +1,64 @@
-import { useState } from 'react'
-
+import React from "react";
 
 function BPiece(props) {
-    var value = props.kind.toUpperCase();
-    var color = "n";
-    if(value === props.kind) {
-        color = "w";
-    } else {
-        color = "b";
-    }
-    
-    return (
-        <img className="w-fill" src={`pieces/${color}${value}.svg`}/>
-    );
+  const value = props.kind.toUpperCase();
+  let color = "n";
+  if (value === props.kind) {
+    color = "w";
+  } else {
+    color = "b";
+  }
+
+  return <img className="w-fill" src={`pieces/${color}${value}.svg`} />;
 }
 
 function BCell(props) {
-
-    return (
-        <div className={`inline-block w-10 h-10 lg:w-14 lg:h-14 ${props.shade == 1 ? "bg-sky-800" : "bg-sky-100"}`}>
-            {props.piece ? <BPiece kind={props.piece}/> : <span/> }
-        </div>
-    );
+  return (
+    <div
+      className={`inline-block w-10 h-10 lg:w-14 lg:h-14 ${
+        props.shade === 1 ? "bg-sky-800" : "bg-sky-100"
+      }`}
+    >
+      {props.piece ? <BPiece kind={props.piece} /> : <span />}
+    </div>
+  );
 }
 
 function Board(props) {
-  let whiteMove = props.whiteMove;
-  let pos = props.position;
+  const whiteMove = props.whiteMove;
+  const pos = props.position;
 
-  var piecesOnBoard = pos.split("/");
-  if(whiteMove == 0){
+  let piecesOnBoard = pos.split("/");
+  if (whiteMove == 0) {
     piecesOnBoard = piecesOnBoard.reverse();
   }
-  
-    piecesOnBoard = piecesOnBoard.map((x)=>{
-    return x.split("").flatMap((z)=> {
-        if(z.charAt(0) >= '0' && z.charAt(0) <= '9') {
-            return Array(z.charAt(0) - '0').fill("");
-        } else {
-            return z;
-        }
+
+  piecesOnBoard = piecesOnBoard.map((x) => {
+    return x.split("").flatMap((z) => {
+      if (z.charAt(0) >= "0" && z.charAt(0) <= "9") {
+        return Array(z.charAt(0) - "0").fill("");
+      } else {
+        return z;
+      }
     });
   });
 
-  let rows = [];
-  
-  for(var i = 0; i < 8; i++) {
-    let squares = [];
-    for(var j = 0; j < 8; j++){
-        squares.push(<BCell shade={(i+j)%2 == whiteMove ? 1 : 0 } piece={piecesOnBoard[i][j]}></BCell>);
+  const rows = [];
+
+  for (let i = 0; i < 8; i++) {
+    const squares = [];
+    for (let j = 0; j < 8; j++) {
+      squares.push(
+        <BCell
+          shade={(i + j) % 2 == whiteMove ? 1 : 0}
+          piece={piecesOnBoard[i][j]}
+        ></BCell>
+      );
     }
     rows.push(<div className="h-10 lg:h-14">{squares}</div>);
   }
 
-  return (
-        <div className="place-self-center p-4">
-            {rows}
-        </div>
-  );
+  return <div className="place-self-center p-4">{rows}</div>;
 }
 
-export default Board
+export default Board;
